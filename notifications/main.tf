3d741a722c9355f7409069ac37ef33e6ab799371
Stuart Ellis <stuart@stuartellis.name>
2021-06-03 22:28:08 +0100

    Add module for notifications

---
+++ notifications/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "= 0.15.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 3.43.0"
+    }
+  }
+
+}
+
+resource "aws_ssm_parameter" "parameter_terraform" {
+  name  = "/stuart-ellis/lab/tf/canary"
+  value = "Terraform active"
+  type  = "String"
+}
