91e27f6a5acf7ddd0f8cd926412107e77c4b95f1
Stuart Ellis <stuart@stuartellis.name>
2021-06-05 22:07:33 +0100

    Add SNS topic

--- aws/notifications/main.tf
+++ aws/notifications/main.tf
@@ -11,9 +11,14 @@ terraform {
 }
 
 resource "aws_ssm_parameter" "parameter_terraform" {
-  name  = "/stuart-ellis/lab/tf/timestamp"
+  name  = "/stuart-ellis/tf/timestamp"
   description = "Timestamp for Terraform run"
   overwrite = true
   value = timestamp()
   type  = "String"
+}
+
+resource "aws_sns_topic" "alerts" {
+  name              = "stuart-ellis-alerts"
+  display_name      = "aws-alerts"
 }
